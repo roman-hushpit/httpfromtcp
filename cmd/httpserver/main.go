14d3efb484b8d52ab68396f461a9cf872722a9ba
Roman <roman.gushpit@gmail.com>
2026-07-04 12:48:31 +0000

    Implement server handler and use custom http request parser

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/roman-hushpit/learn-http-protocol/internal/request"
+	"github.com/roman-hushpit/learn-http-protocol/internal/response"
 	"github.com/roman-hushpit/learn-http-protocol/internal/server"
 )
 
 const port = 42069
 
 func main() {
-	server, err := server.Serve(port)
+	server, err := server.Serve(port, func(w io.Writer, req *request.Request) *server.HandlerError {
+		if req.RequestLine.RequestTarget == "/yourproblem" {
+			return &server.HandlerError{
+				Message:    "Your problem is not my problem\n",
+				StatusCode: response.BAD_REQUEST,
+			}
+		}
+		if req.RequestLine.RequestTarget == "/myproblem" {
+			return &server.HandlerError{
+				Message:    "Woopsie, my bad\n",
+				StatusCode: response.INTERNAL_SERVER_ERROR,
+			}
+		}
+		w.Write([]byte("All good, frfr\n"))
+		return nil
+	})
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,12 +1,15 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"strconv"
 	"sync/atomic"
 
+	"github.com/roman-hushpit/learn-http-protocol/internal/request"
 	"github.com/roman-hushpit/learn-http-protocol/internal/response"
 )
 
@@ -14,9 +17,10 @@ type Server struct {
 	Port     int
 	Listener net.Listener
 	Closed   atomic.Bool
+	Handler  Handler
 }
 
-func Serve(port int) (*Server, error) {
+func Serve(port int, handler Handler) (*Server, error) {
 	listener, err := net.Listen("tcp", ":"+strconv.Itoa(port))
 	if err != nil {
 		return nil, err
@@ -24,6 +28,7 @@ func Serve(port int) (*Server, error) {
 	server := &Server{
 		Port:     port,
 		Listener: listener,
+		Handler:  handler,
 	}
 
 	go server.listen()
@@ -58,12 +63,54 @@ func (s *Server) listen() {
 
 func (s *Server) handle(conn net.Conn) {
 	defer conn.Close()
-	err := response.WriteStatusLine(conn, 200)
+	request, err := request.RequestFromReader(conn)
+	if err != nil {
+		hErr := &HandlerError{
+			StatusCode: response.BAD_REQUEST,
+			Message:    err.Error(),
+		}
+		hErr.WriteError(conn)
+		return
+	}
+	var b bytes.Buffer
+	handlerEror := s.Handler(&b, request)
+	if handlerEror != nil {
+		handlerEror.WriteError(conn)
+		return
+	}
+
+	err = response.WriteStatusLine(conn, 200)
 	if err != nil {
 		return
 	}
-	err = response.WriteHeaders(conn, response.GetDefaultHeaders(0))
+	err = response.WriteHeaders(conn, response.GetDefaultHeaders(len(b.Bytes())))
+	if err != nil {
+		return
+	}
+	_, err = conn.Write(b.Bytes())
 	if err != nil {
 		return
 	}
 }
+
+type HandlerError struct {
+	StatusCode response.StatusCode
+	Message    string
+}
+
+func (he *HandlerError) WriteError(out io.Writer) {
+	err := response.WriteStatusLine(out, he.StatusCode)
+	if err != nil {
+		return
+	}
+	err = response.WriteHeaders(out, response.GetDefaultHeaders(len([]byte(he.Message))))
+	if err != nil {
+		return
+	}
+	_, err = out.Write([]byte(he.Message))
+	if err != nil {
+		return
+	}
+}
+
+type Handler func(w io.Writer, req *request.Request) *HandlerError
